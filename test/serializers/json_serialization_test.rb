3744ead416e9b390d634ff223056e02d9e1932a5
Benjamin Fleischer <github@benjaminfleischer.com>
2016-01-03 23:07:12 -0600

    Add failing scenarios

--- test/serializers/json_serialization_test.rb
+++ test/serializers/json_serialization_test.rb
@@ -10,6 +10,7 @@ class ActiveModel::Serializer
     end
     class BlogSerializer < ActiveModel::Serializer
       attributes :id
+      attribute :name, key: :title
 
       has_many :authors
     end
@@ -24,7 +25,7 @@ class ActiveModel::Serializer
     end
 
     def test_serializer_to_json
-      expected = { id: 2 }
+      expected = { id: 2, title: 'The Blog' }
       expected_json = '{"id":2}'
       assert_equal(expected, @serializer.serializable_hash)
       assert_equal(expected, @serializer.as_json)
