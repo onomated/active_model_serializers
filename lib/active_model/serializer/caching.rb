d5b24d830d86a42818b6c29cb6dfd5489f0b93ef
Benjamin Fleischer <github@benjaminfleischer.com>
2016-04-13 22:44:27 -0500

    fragment cache enabled

--- lib/active_model/serializer/caching.rb
+++ lib/active_model/serializer/caching.rb
@@ -151,7 +151,6 @@ module ActiveModel
         end
 
         def fragment_cache_enabled?
-          return false
           perform_caching? && cache_store &&
             (_cache_only && !_cache_except || !_cache_only && _cache_except)
         end
