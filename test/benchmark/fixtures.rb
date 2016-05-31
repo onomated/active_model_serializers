a38e5efc303497ac841416f76b87ee32d09034c1
Benjamin Fleischer <github@benjaminfleischer.com>
2016-05-31 00:24:24 -0500

    Benchmark with cache that supports read-multi
    Benchmark results:
    {
      "commit_hash": "a5efac1",
      "version": "0.10.0",
      "rails_version": "4.2.6",
      "benchmark_run[environment]": "2.2.3p173",
      "runs": [
        {
          "benchmark_type[category]": "caching on: caching serializers: gc off",
          "benchmark_run[result][iterations_per_second]": 573.413,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 1530
        },
        {
          "benchmark_type[category]": "caching on: fragment caching serializers: gc off",
          "benchmark_run[result][iterations_per_second]": 479.14,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 1904
        },
        {
          "benchmark_type[category]": "caching on: non-caching serializers: gc off",
          "benchmark_run[result][iterations_per_second]": 946.236,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 1276
        },
        {
          "benchmark_type[category]": "caching off: caching serializers: gc off",
          "benchmark_run[result][iterations_per_second]": 508.987,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 1530
        },
        {
          "benchmark_type[category]": "caching off: fragment caching serializers: gc off",
          "benchmark_run[result][iterations_per_second]": 430.95,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 1904
        },
        {
          "benchmark_type[category]": "caching off: non-caching serializers: gc off",
          "benchmark_run[result][iterations_per_second]": 840.046,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 1276
        }
      ]
    }
    
    Benchmark results:
    {
      "commit_hash": "a5efac1",
      "version": "0.10.0",
      "rails_version": "4.2.6",
      "benchmark_run[environment]": "2.2.3p173",
      "runs": [
        {
          "benchmark_type[category]": "camel",
          "benchmark_run[result][iterations_per_second]": 466.203,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 3569
        },
        {
          "benchmark_type[category]": "camel_lower",
          "benchmark_run[result][iterations_per_second]": 609.085,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 2487
        },
        {
          "benchmark_type[category]": "dash",
          "benchmark_run[result][iterations_per_second]": 2999.977,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 719
        },
        {
          "benchmark_type[category]": "unaltered",
          "benchmark_run[result][iterations_per_second]": 6980797.879,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 1
        },
        {
          "benchmark_type[category]": "underscore",
          "benchmark_run[result][iterations_per_second]": 4700.885,
          "benchmark_run[result][total_allocated_objects_per_iteration]": 313
        }
      ]
    }

--- test/benchmark/fixtures.rb
+++ test/benchmark/fixtures.rb
@@ -48,13 +48,13 @@ end
 Rails.configuration.serializers << CachingAuthorSerializer
 
 class CachingCommentSerializer < CommentSerializer
-  cache expires_in: 1.day, skip_digest: true
+  cache expires_in: 1.day.to_i, skip_digest: true
 end
 Rails.configuration.serializers << CachingCommentSerializer
 
 # see https://github.com/rails-api/active_model_serializers/pull/1690/commits/68715b8f99bc29677e8a47bb3f305f23c077024b#r60344532
 class CachingPostSerializer < ActiveModel::Serializer
-  cache key: 'post', expires_in: 0.1, skip_digest: true
+  cache key: 'post', expires_in: 1.day.to_i, skip_digest: true
 
   attributes :id, :title, :body
 
@@ -89,7 +89,7 @@ Rails.configuration.serializers << CachingCommentSerializer
 
 # see https://github.com/rails-api/active_model_serializers/pull/1690/commits/68715b8f99bc29677e8a47bb3f305f23c077024b#r60344532
 class FragmentCachingPostSerializer < ActiveModel::Serializer
-  cache key: 'post', expires_in: 0.1, skip_digest: true
+  cache key: 'post', expires_in: 1.day.to_i, skip_digest: true
 
   attributes :id, :title, :body
 
